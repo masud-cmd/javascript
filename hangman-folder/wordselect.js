c3987cb6793bf431d288c464c8a6b486a3d61740
Felipe Fernandez <fettoviktig@gmail.com>
2023-11-22 15:07:23 +0100

    loopar och listor

---
+++ hangman-folder/wordselect.js
@@ -0,0 +1,24 @@
+import words from "./svenska-ord.js";
+
+function easyPeasy(words) {
+    const easyWords = words.filter(word => word.length === 10)
+    if (easyWords) {
+        const easyIndex = Math.floor(Math.random() * easyWords.length)
+        return easyWords[easyIndex]
+    }
+}
+
+function mediumSquezy(words) {
+    const mediumWords = words.filter(word => word.length === 7)
+    if (mediumWords) {
+        const mediumIndex = Math.floor(Math.random() * mediumWords.length)
+        return mediumWords[mediumIndex]
+    }
+}
+function hardyParty(words) {
+    const hardWords = words.filter(word => word.length === 5)
+    if (hardWords) {
+        const hardIndex = Math.floor(Math.random() * hardWords.length)
+        return hardWords[hardIndex]
+    }
+}
