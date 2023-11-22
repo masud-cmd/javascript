c4c9705799c0883fbb3a0496aba2a012b4e09643
Jacob Mounir <143597510+Jacob-Mounir@users.noreply.github.com>
2023-11-22 09:07:12 +0100

    Skapade en test folder för att leka runt i

---
+++ Test Jacob/script.js
@@ -0,0 +1,29 @@
+
+/*
+
+---- TO DO ------
+
+1. Länka Play knappen så att det sparar username + nivå samt bytter section(ändrar css klass till Display: none)
+
+
+*/
+
+//Knapp som döljer första sidan
+//Saknar: Spara ner värdet
+const startPage = document.querySelector('.start')
+
+const play = document.querySelector('#play-btn')
+
+
+play.addEventListener('click', () => {
+
+	startPage.classList.remove('start')
+	startPage.classList.add('invisible')
+
+	//Osäker på dessa men en  skiss
+	localStorage.setItem("Name", input.name);
+	localStorage.setItem("Level", input.level);
+
+
+
+})
